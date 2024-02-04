775987e5880ce8d6c66a54f24df0781bf5703456
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2024-02-04 11:13:52 -0300

    Initial Commit

---
+++ LocalTennis/LocalTennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalTennisApp.swift
+//  LocalTennis
+//
+//  Created by Rodrigo Alonso on 04-02-24.
+//
+
+import SwiftUI
+
+@main
+struct LocalTennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
