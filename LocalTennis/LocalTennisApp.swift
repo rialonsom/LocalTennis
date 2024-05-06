1febaefaebcbe6de2313da460dba6415e6bd7a87
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2024-05-05 20:34:39 -0400

    Add tasks to load and save data from store

--- LocalTennis/LocalTennisApp.swift
+++ LocalTennis/LocalTennisApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct LocalTennisApp: App {
+    @StateObject var localTennisManager = LocalTennisManager()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() {
+                Task {
+                    do {
+                        try await localTennisManager.saveData()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+            .environmentObject(localTennisManager)
+            .task {
+                do {
+                    try await localTennisManager.loadData()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+                
+            }
         }
     }
 }
