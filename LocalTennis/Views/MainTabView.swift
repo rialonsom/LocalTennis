57aa4142f8b57cc208dd7897eb1d9425de284bd1
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2024-05-12 21:20:59 -0400

    Disable match creation if not enough players (2)

--- LocalTennis/Views/MainTabView.swift
+++ LocalTennis/Views/MainTabView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 struct MainTabView: View {
     @State private var selectedTab = 0
     @State private var isShowingMatchSheet: Bool = false
+    @State private var isShowingNewMatchAlert: Bool = false
+    @EnvironmentObject var localTennisManager: LocalTennisManager
+    private let newMatchAlertTitle = "You need to create at least two players before playing a match."
     
     var body: some View {
+        let newMatchEnabled = localTennisManager.players.count >= 2
+        
         ZStack(alignment: .bottom) {
             TabView(selection: $selectedTab) {
                 Group {
@@ -40,7 +45,7 @@ struct MainTabView: View {
             Button(action: {
                 isShowingMatchSheet = true
             }, label: {
-                Image(systemName: "plus.circle.fill")
+                Image(systemName: "tennisball.fill")
                     .resizable()
                     .tint(.red)
                     .background(.white)
@@ -48,6 +53,19 @@ struct MainTabView: View {
             .frame(width: 50, height: 50)
             .clipShape(Circle())
             .padding(.bottom, 20)
+            .disabled(!newMatchEnabled)
+            .onTapGesture {
+                if (!newMatchEnabled) {
+                    isShowingNewMatchAlert = true
+                }
+            }
+            .alert(newMatchAlertTitle, isPresented: $isShowingNewMatchAlert) {
+                Button(action: {
+                    isShowingNewMatchAlert = false
+                }, label: {
+                    Text("Ok")
+                })
+            }
         }
     }
 }
@@ -57,6 +75,16 @@ struct MainTabView: View {
     let localTennisManager = LocalTennisManager(
         matches: Match.exampleHistoryMatches,
         players: Player.examplePlayers
+    )
+    
+    return MainTabView()
+        .environmentObject(localTennisManager)
+}
+
+#Preview {
+    let localTennisManager = LocalTennisManager(
+        matches: Match.exampleHistoryMatches,
+        players: []
     )
     
     return MainTabView()
