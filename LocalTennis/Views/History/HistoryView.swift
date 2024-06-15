fa4ec1edb2a81a32e2c2be6f6d455ae0864f8907
Rodrigo Alonso <rodrigoignacioalonso@gmail.com>
2024-06-15 18:07:22 -0400

    Implement empty state views for Matches and Players

--- LocalTennis/Views/History/HistoryView.swift
+++ LocalTennis/Views/History/HistoryView.swift
@@ -34,12 +34,39 @@ struct HistoryView: View {
         }
         .listStyle(.plain)
         .navigationTitle("History")
+        .overlay {
+            if (localTennisManager.matches.isEmpty) {
+                VStack {
+                    Image(systemName: "tennis.racket")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.horizontal, 90)
+                        .foregroundStyle(.accent)
+                    Text("No matches")
+                        .font(.title)
+                    Text("You can start playing by tapping on the tennis ball icon below")
+                }
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 50)
+            }
+        }
     }
 }
 
 #Preview {
     let localTennisManager = LocalTennisManager(
         matches: Match.exampleHistoryMatches
+    )
+    
+    return NavigationStack {
+        HistoryView()
+            .environmentObject(localTennisManager)
+    }
+}
+
+#Preview {
+    let localTennisManager = LocalTennisManager(
+        matches: []
     )
     
     return NavigationStack {
--- LocalTennis/Views/Players/PlayersView.swift
+++ LocalTennis/Views/Players/PlayersView.swift
@@ -37,12 +37,39 @@ struct PlayersView: View {
         .sheet(isPresented: $isShowingNewPlayerSheet, content: {
             NewPlayerSheetView()
         })
+        .overlay {
+            if (localTennisManager.players.isEmpty) {
+                VStack {
+                    Image(systemName: "tennis.racket")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundStyle(.accent)
+                        .padding(.horizontal, 90)
+                    Text("No players")
+                        .font(.title)
+                    Text("You can add new players by tapping the + icon at the top right corner")
+                }
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 50)
+            }
+        }
     }
 }
 
 #Preview {
     let localTennisManager = LocalTennisManager(
         players: Player.examplePlayers
+    )
+    
+    return NavigationStack {
+        PlayersView()
+            .environmentObject(localTennisManager)
+    }
+}
+
+#Preview {
+    let localTennisManager = LocalTennisManager(
+        players: []
     )
     
     return NavigationStack {
